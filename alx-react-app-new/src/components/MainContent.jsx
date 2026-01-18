4894f17eee8bf071a37cd86f2015a44efe467eae
Nasiru Bawa <haskenasir8@gmail.com>
2026-01-18 17:10:09 +0000

    Added inline styling to the components Header, footer, Main Content and User profile

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f0f0', padding: '15px', margin: '10px', textAlign: 'center' }}>
+      <p style={{ fontSize: '18px', color: 'darkslategray' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
