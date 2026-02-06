84a4943296f92f981c84e92ff0d18e2b0a8bf899
nas-bawa <haskenasir8@gmail.com>
2026-02-06 19:29:39 +0000

    feat: integrate GitHub API with search component

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -36,7 +36,7 @@ function Search() {
 
       {/* Conditional rendering */}
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user</p>}
+      {error && <p>["Looks like we cant find the user"]</p>}
       {user && (
         <div style={{ marginTop: "1rem" }}>
           <img src={user.avatar_url} alt={user.login} width="100" />
