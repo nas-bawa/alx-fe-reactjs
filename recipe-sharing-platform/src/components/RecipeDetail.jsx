b67e36afd5f429e3c0088eb8b8c73df66efd13f6
nas-bawa <haskenasir8@gmail.com>
2026-02-14 20:38:59 +0000

    Added another page, Recipe Detail and also using route for dynamic surfing

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,9 +1,16 @@
 import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import recipesData from "../data.json";
 
 function RecipeDetail() {
   const { id } = useParams();
-  const recipe = recipesData.find((r) => r.id === parseInt(id));
+  const [recipe, setRecipe] = useState(null);
+
+  useEffect(() => {
+    // Simulate fetching data (from JSON file)
+    const foundRecipe = recipesData.find((r) => r.id === parseInt(id));
+    setRecipe(foundRecipe);
+  }, [id]);
 
   if (!recipe) {
     return (
